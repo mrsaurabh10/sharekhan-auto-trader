94f82d9f49460677aaec485c307938258aa4bb94
sngupta <saurabh.f.gupta@oracle.com>
2025-07-14 08:48:23 +0530

    allow api

--- src/main/java/org/com/sharekhan/security/SecurityConfig.java
+++ src/main/java/org/com/sharekhan/security/SecurityConfig.java
@@ -14,7 +14,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/h2-console/**").permitAll()
+                .requestMatchers("/h2-console/**", "/api/**").permitAll()
                 .anyRequest().authenticated()
             )
             .csrf(csrf -> csrf.disable()) // Disable CSRF for dev
